afb88c96754b8a84ce3888f1df2f56089c2e50f5
Yusuke Nishimura <yusuke.nishimura@gmail.com>
2018-08-22 15:43:17 +0900

    init version

---
+++ yaml2db.rb
@@ -0,0 +1,279 @@
+# coding: utf-8
+
+require 'yaml'
+require 'optparse'
+
+INDENT = " " * 2
+CHECK_COLUMN_NAME = /<name>/ # 列内チェック制約の場合に列名に変換する値を示す
+CHECK_REPLACE_COLUMN_NAME = /<([^\s<>]+)>/ # 列外チェック制約の場合に列名に変換する値を示す
+
+module YAMLLoadable
+  def extract_element(parsetree, element, raise_when_noexist = true)
+    raise 'This root element is not a Hash Object.' if parsetree.instance_of?(Array)
+    if (ret = parsetree[element])
+      ret
+    else
+      raise "#{element} element doesn't exist." if raise_when_noexist
+      nil
+    end
+  end
+
+  def self.load_yaml(filename)
+    (filename ? YAML.load_file(filename) : YAML.load(ARGF))
+  end
+end
+
+class CheckConstraints
+  class Check
+    def initialize(counter, notation, column)
+      @counter = counter
+      @notation = notation
+      @column = column
+    end
+  end
+  def initialize(table)
+    @table = table
+    @checks = []
+  end
+  def add(notation, column = nil)
+    @checks.push(CheckConstraints::Check.new(@checks.count, notation, column))
+    @checks[-1]
+  end
+end
+
+class LoadableElement
+  include YAMLLoadable 
+
+  def initialize(parsetree, filename = nil)
+    @filename = filename
+    @root = extract_element(parsetree, self.class.to_s.downcase)
+  end
+
+  # ファイルからロードする場合はこのクラスメソッドを使用する。
+  def self.load(filename)
+    new(YAMLLoadable::load_yaml(filename), filename)
+  end
+
+  def [](key)
+    @root[key]
+  end
+
+  def dump(node = @root)
+    YAML.dump(node)
+  end
+
+end
+
+class Domains < LoadableElement
+end
+
+class Dictionary < LoadableElement
+end
+
+class Column
+  attr_reader :name, :pname, :pkey, :type, :size, :default, :nullable, :comment, :indexes, :oname
+  def initialize(parsetree, dict = nil, domains = nil)
+    @root = parsetree
+    @domain = @root["domain"]
+    @oname = @root["name"] # Original name
+    @name = @oname.delete('.') if @root["name"]
+    merge_attribute(domains, @root["type"]) if domains
+    @pname = @root["pname"] || make_pname(dict)
+    @pkey = @root["pkey"]
+    @type = @root["type"]
+    @checks = @root["check"]
+    @size = @root["size"]
+    @default = @root["default"]
+    @nullable = @root.has_key?("nullable") ? @root["nullable"] : true
+    @comment = @root["comment"] || ""
+    @indexes = @root.select{|key, val| key =~ /\Aindex\d+\Z/}
+  end
+
+  def checks
+    return nil unless @checks
+    @checks = [@checks] if !@checks.instance_of?(Array)
+    return @checks.map do |e|
+      if e.match(CHECK_COLUMN_NAME)
+        e.gsub(CHECK_COLUMN_NAME, @pname)
+      else
+        @pname + ' ' + e
+      end
+    end
+  end 
+
+  private
+
+  def make_pname(dict)
+    return nil unless dict
+    # original_nameを.で分割し、dictで変換
+    @oname.split('.').inject(""){|r, e| r + (dict[e] || e)} if @oname
+  end
+
+  def merge_attribute(domains, type)
+    # domain属性があれば、ドメインの定義を取得し、ドメインの属性で上書きする
+    if @domain && (domain_elements = domains[@domain])
+      @root = domain_elements.merge(@root)
+    # type属性がない場合はname属性からドメイン定義を取得する。
+    elsif !type && (domain_elements = domains[@name])
+      @root = domain_elements.merge(@root)
+    end
+  end
+
+end
+
+class Table < LoadableElement
+  attr_reader :columns
+
+  def initialize(parsetree, filename, dict, domains)
+    super(parsetree, filename)
+    @columns = extract_element(@root, 'columns').map do |e|
+      Column.new(e, dict, domains)
+    end
+
+    @checks = @root['checks']
+  end
+
+  def self.load(filename, dict, domains)
+    new(YAMLLoadable::load_yaml(filename), filename, dict, domains)
+  end
+
+  def name
+    @root["name"]
+  end
+
+  def pname
+    @root["pname"] || File.basename(@filename, ".*")
+  end
+
+  def pkey
+    @columns.select{|e| e.pkey}.sort{|x, y| x.pkey <=> y.pkey}
+  end
+
+  def index_keys
+    keys = @columns.inject([]){|r, v| r + v.indexes.keys}.uniq.sort
+  end
+
+  def index(key)
+    @columns.select{|e| e.indexes[key]}.sort{|x, y| x.indexes[key] <=> y.indexes[key]}
+  end
+
+  def comment
+    @root["comment"] || ""
+  end
+
+  def checks
+    return nil unless @checks
+    @checks = [@checks] if !@checks.instance_of?(Array)
+    return @checks.map do |e|
+      e.gsub(CHECK_REPLACE_COLUMN_NAME) do |column_name|
+        checked_column = @columns.find(nil){|column| column.oname == $1}
+        if checked_column
+          checked_column.pname
+        else
+          ''
+        end
+      end
+    end
+  end
+
+  
+end
+
+module Sql
+  def get_ddl_table(table)
+
+<<EOS
+drop table #{table.pname};
+create table #{table.pname} (
+#{get_columns_def(table.columns, 1)}
+#{get_tab_constraints(table, 1)}
+);
+EOS
+
+  end
+  def format_comment(comment)
+    if comment == ""
+      return ""
+    else
+      return "\t " + comment
+    end
+  end
+  def get_ddl_table_comment(table, indent = 0)
+    
+    result = <<EOS
+#{INDENT * indent}comment on table #{table.pname} is '#{table.name}#{format_comment(table.comment)}';
+EOS
+    table.columns.each do |e|
+      result += <<EOS
+#{INDENT * indent}comment on column #{table.pname}.#{e.pname} is '#{e.name}#{format_comment(e.comment)}';
+EOS
+    end
+  result
+  end
+
+  def get_tab_constraints(table, indent = 0)
+    ret_str = ""
+    if table.pkey.size > 0
+      ret_str = INDENT * indent + ", constraint PK_#{table.pname} primary key(#{table.pkey.map{|e| e.pname}.join(", ")})"
+    end
+    ret_str += get_check(table, "\n" + INDENT * indent + ", ")
+  end
+
+  def get_index(table)
+    table.index_keys.inject("") do |r, e|
+      r + "create index I_#{table.pname}#{e.match(/\d+$/)[0]} on #{table.pname}(#{table.index(e).map{|col| col.pname}.join(", ")});\n"
+    end
+  end
+
+  def get_check(checked_obj, sepstr = " ")
+    return "" unless (checked_obj.checks)
+    checked_obj.checks.inject(""){|r, i| r + sepstr + "CHECK(#{i})"}
+  end
+
+  def get_size(column)
+    if (val = column.size)
+      "(" + val.to_s + ")"
+    else
+        ""
+    end
+  end
+
+  def get_default(column)
+    if (val = column.default)
+      "DEFAULT #{val}"
+    else
+       ""
+    end
+  end
+
+  def get_null(column)
+    (column.nullable ? "" : " NOT NULL") 
+  end
+
+  def get_columns_def(columns, indent = 0)
+    (columns.map do |e|
+        INDENT * indent + "#{e.pname} #{e.type}#{get_size(e)}"  +
+        " #{get_default(e)}#{get_null(e)}#{get_check(e)}"
+    end).join(",\n")
+  end
+
+end
+
+if __FILE__ == $PROGRAM_NAME
+  include Sql
+  params = ARGV.getopts("", "dic:", "dom:", "tab:", "index")
+  script_path = File.expand_path(File.dirname($0))
+
+  tab = Table.load(
+    params["tab"] || 'table.yaml', 
+    Dictionary.load(params["dic"] || script_path + '/dict.yaml'),
+    Domains.load(params["dom"] || script_path + '/domains.yaml')
+  )
+  if params["index"]
+    puts Sql::get_index(tab).encode('cp932') if tab.index_keys.size > 0
+  else
+    puts Sql::get_ddl_table(tab).encode('cp932')
+    puts Sql::get_ddl_table_comment(tab).encode('cp932')
+  end
+
+end
---
+++ yaml2db_unit.rb
@@ -0,0 +1,184 @@
+require 'test/unit'
+require 'yaml'
+require_relative 'create_table'
+require 'pp'
+
+class TestSql_Column < Test::Unit::TestCase
+  include Sql
+
+  EMPTY_COLUMN_YAML = "error: error"
+  PROPER_COLUMN_YAML = "{size: 100, default: 10, nullable: true, check: ['>= 100'], pname: PROP}"
+
+  def setup
+    @empty_data = Column.new(YAML.load(EMPTY_COLUMN_YAML))
+    @proper_data = Column.new(YAML.load(PROPER_COLUMN_YAML))
+  end
+
+  def test_get_size
+    assert_equal "", get_size(@empty_data)
+    assert_equal "(100)", get_size(@proper_data)
+  end
+
+  def test_get_default
+    assert_equal "", get_default(@empty_data)
+    assert_equal "DEFAULT 10", get_default(@proper_data)
+  end
+
+  def test_get_null
+    assert_equal "", get_null(@empty_data)
+    assert_equal "", get_null(@proper_data)
+  end
+  
+  def test_get_check
+    assert_equal "", get_check(@empty_data)
+    assert_equal " CHECK(PROP >= 100)", get_check(@proper_data)
+
+    proper_data2 = Column.new(YAML.load("{pname: PROP, check: ['>= 100', '<= 1000']}"))
+    assert_equal " CHECK(PROP >= 100) CHECK(PROP <= 1000)", get_check(proper_data2)
+
+  end
+
+end
+
+class TestSql_Columns < Test::Unit::TestCase
+  include Sql, YAMLLoadable
+
+  def setup
+    @domains = Domains.new(
+      YAML.load(
+        "domains: {domain1: {type: number, size: 10, default: 200, nullable: false}}"))
+    @dict = Dictionary.new(
+      YAML.load(
+        "{dictionary: {word1: AAA, word2: BBB, word3: CCC}}"
+      )
+    )
+  end
+
+  def load_columns(parsetree, dict = nil, domains = nil)
+    @columns = extract_element(parsetree, 'columns').map do |e|
+      Column.new(e, dict, domains)
+    end
+  end
+
+  def test_no_column
+    assert_equal nil, extract_element(YAML.load("foo: bar"), 'columns', false)
+    assert_raise(RuntimeError){extract_element(YAML.load("foo: bar"), 'columns')}
+    assert_raise(RuntimeError){extract_element(YAML.load("[1,2,3]"), 'columns', false)}
+  end
+
+  def test_one_column
+    load_columns YAML.load("columns: [{pname: TEST1, type: number, size: 4, default: 100, nullable: true, check: ['>= 100', 'nvl(<name>, 0) <= 1000']}]")
+    assert_equal "TEST1 number(4) DEFAULT 100 CHECK(TEST1 >= 100) CHECK(nvl(TEST1, 0) <= 1000)", get_columns_def(@columns)
+  end
+
+  def test_columns
+    load_columns YAML.load("columns: [{pname: TEST1, type: date, default: sysdate, nullable: false}, {pname: TEST2, type: varchar2, default: '''SP''', check: ['>= ''100''']}]")
+    assert_equal "TEST1 date DEFAULT sysdate NOT NULL,\nTEST2 varchar2 DEFAULT 'SP' CHECK(TEST2 >= '100')", get_columns_def(@columns)
+  end
+
+  def test_one_column_domain
+    load_columns YAML.load("columns: [{pname: TEST1, domain: domain1, check: '<name> >= 100'}]"), nil, @domains
+    assert_equal "TEST1 number(10) DEFAULT 200 NOT NULL CHECK(TEST1 >= 100)", get_columns_def(@columns)
+
+  end
+
+  def test_domain_name_column
+    load_columns YAML.load("columns: [{pname: TEST1, name: domain1}]"), nil, @domains
+    assert_equal "TEST1 number(10) DEFAULT 200 NOT NULL", get_columns_def(@columns)
+  end
+
+  def test_make_pname_with_dict
+    load_columns YAML.load("{columns: [{name: word1, type: number, nullable: true}]}"), @dict
+    assert_equal "AAA number ", get_columns_def(@columns)
+
+    load_columns YAML.load("{columns: [{name: word1.word2, type: number, nullable: true}]}"), @dict
+    assert_equal "AAABBB number ", get_columns_def(@columns)
+
+    load_columns YAML.load("{columns: [{pname: word1.word2, type: number, nullable: true}]}"), @dict
+    assert_equal "word1.word2 number ", get_columns_def(@columns)
+  end
+
+  def test_indexes
+    load_columns YAML.load(
+      "{columns: [{name: word1, type: number, nullable: true, index2: 2, index1: 1}, {name: word2, type: number, index2: 1}, {name: word3, type: number}]}"
+    ), @dict
+    assert_equal 2, @columns[0].indexes["index2"]
+    assert_equal 1, @columns[0].indexes["index1"]
+    assert_equal 1, @columns[1].indexes["index2"]
+  end
+
+
+end
+
+class TestTable < Test::Unit::TestCase
+  include Sql, YAMLLoadable
+
+  TABLE_SAMPLE = "{table: {name: testname, pname: test_table, columns: [{ name: word1.word2, domain: domain1}]}}"
+  TABLE_SAMPLE_NOPNAME = "{table: {name: testname, columns: [{ name: word1.word2, domain: domain1}]}}"
+  TABLE_SAMPLE_COLUMNS = "{table: {name: testname, pname: test_table, columns: [{ name: word1.word2, domain: domain1, nullable: false}, {name: word2.word3, domain: domain2, pkey: 2, nullable: false}, {name: word3.GGG, domain: domain1, pkey: 1, nullable: false}]}}"
+  def setup
+    @domains = Domains.new(
+      YAML.load(
+        "domains: {domain1: {type: number, size: 10, default: 200, nullable: false}, domain2: {type: varchar, size: 10}}"))
+    @dict = Dictionary.new(
+      YAML.load(
+        "{dictionary: {word1: AAA, word2: BBB, word3: CCC}}"
+      )
+    )
+  end
+
+  def test_make_table
+    table = Table.new(YAML.load(TABLE_SAMPLE), nil, @dict, @domains)
+    assert_equal "AAABBB number(10) DEFAULT 200 NOT NULL", get_columns_def(table.columns)
+    assert_equal "test_table", table.pname
+
+    table = Table.new(YAML.load(TABLE_SAMPLE_NOPNAME), 'file_name.yaml', @dict, @domains)
+    assert_equal "file_name", table.pname
+
+  end
+
+  def test_get_pkey
+    table = Table.new(YAML.load(TABLE_SAMPLE_COLUMNS), nil, @dict, @domains)
+    assert_equal "CCCGGG", table.pkey[0].pname
+    assert_equal "BBBCCC", table.pkey[1].pname
+  end
+
+  def test_make_create_table
+    table = Table.new(YAML.load(TABLE_SAMPLE), nil, @dict, @domains)
+    assert_equal "drop table test_table;\ncreate table test_table (\n  AAABBB number(10) DEFAULT 200 NOT NULL\n\n);\n", get_ddl_table(table)
+
+    table = Table.new(YAML.load(TABLE_SAMPLE_COLUMNS), nil, @dict, @domains)
+    assert_equal "drop table test_table;\ncreate table test_table (\n  AAABBB number(10) DEFAULT 200 NOT NULL,\n  BBBCCC varchar(10)  NOT NULL,\n  CCCGGG number(10) DEFAULT 200 NOT NULL\n  , constraint PK_test_table primary key(CCCGGG, BBBCCC)\n);\n", get_ddl_table(table)
+  end
+
+  def test_make_table_comment
+    table = Table.new(YAML.load(TABLE_SAMPLE_COLUMNS), nil, @dict, @domains)
+    assert_equal "comment on table test_table is 'testname';\ncomment on column test_table.AAABBB is 'word1word2';\ncomment on column test_table.BBBCCC is 'word2word3';\ncomment on column test_table.CCCGGG is 'word3GGG';\n", get_ddl_table_comment(table)
+  end
+  
+  def test_indexes
+    index_test_table =
+      "table: {name: test_index, pname: test_index, columns: [{pname: col1, type: number, nullable: true, index2: 2, index1: 1}, {pname: col2, type: number, index2: 1}, {pname: col3, type: number}]}"
+    table = Table.new(YAML.load(index_test_table), nil, nil, nil)
+    assert_equal "index1", table.index_keys[0]
+    assert_equal "index2", table.index_keys[1]
+    assert_equal "col1", table.index("index1").map{|e| e.pname}.join(",")
+    assert_equal "col2,col1", table.index("index2").map{|e| e.pname}.join(",")
+    assert_equal "create index I_test_index1 on test_index(col1);\ncreate index I_test_index2 on test_index(col2, col1);\n", get_index(table)
+
+  end
+
+  def test_table_checks
+    table_with_check =  "{table: {name: testname, pname: test_table, columns: [{ name: word1.word2, domain: domain1}], checks: <word1.word2> > 0}}"
+    table = Table.new(YAML.load(table_with_check), nil, @dict, @domains)
+    assert_equal "AAABBB > 0", table.checks[0]
+
+    table_with_check2 =  "{table: {name: testname, pname: test_table, columns: [{ name: word1.word2, domain: domain1, nullable: false}, {name: word2.word3, domain: domain2, pkey: 2, nullable: false}, {name: word3.GGG, domain: domain1, pkey: 1, nullable: false}], checks: [<word2.word3> = <word3.GGG>, <word3.GGG> < <word1.word2>]}}"
+    table = Table.new(YAML.load(table_with_check2), nil, @dict, @domains)
+    assert_equal "BBBCCC = CCCGGG", table.checks[0]
+    assert_equal "CCCGGG < AAABBB", table.checks[1]
+    assert_equal "drop table test_table;\ncreate table test_table (\n  AAABBB number(10) DEFAULT 200 NOT NULL,\n  BBBCCC varchar(10)  NOT NULL,\n  CCCGGG number(10) DEFAULT 200 NOT NULL\n  , constraint PK_test_table primary key(CCCGGG, BBBCCC)\n  , CHECK(BBBCCC = CCCGGG)\n  , CHECK(CCCGGG < AAABBB)\n);\n", get_ddl_table(table)
+
+  end
+
+end
